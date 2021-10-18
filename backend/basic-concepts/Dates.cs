129ddd076675204d2eb208f47dbe8fc04383ea62
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2021-10-17 22:16:49 -0300

    dates

---
+++ backend/basic-concepts/Dates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace basic_concepts
+{
+    class Dates
+    {
+        public void DatesTypes()
+        {
+            DateTime data = new DateTime(2015, 3, 30); 
+
+            Console.WriteLine(data); //30/03/2015 00:00:00
+            Console.WriteLine(data.ToLongDateString()); //segunda-feira, 30 de março de 2015
+            Console.WriteLine(data.ToString("dd . yyyy . MM")); //30 . 2015 . 03
+            Console.WriteLine(data.ToString("ddyyyyMM")); //30201503
+            Console.WriteLine(data.ToString("ddyyyyMM - hh:mm:ss")); //30201503 - 12:00:00
+
+            Console.WriteLine(DateTime.Today); // imprime data atual => 17/10/2021 00:00:00
+            Console.WriteLine(DateTime.Now); // imprime data atual c/ horario => 17/10/2021 22:11:00
+
+            Console.WriteLine(DateTime.Today.AddDays(2)); // adianta 2 dias da data atual => 19/10/2021 00:00:00
+            Console.WriteLine(DateTime.Today.AddDays(-2)); // remove 2 dias da data atual => 15/10/2021 00:00:00
+
+            Console.WriteLine((DateTime.Today - new DateTime(2021,1,1)).Days); // diferenca de datas em dias => 88
+
+            var calculo = DateTime.Today - TimeSpan.FromDays(2);
+            Console.WriteLine(calculo);
+
+            Console.Read();
+        }
+    }
+}
--- backend/basic-concepts/Program.cs
+++ backend/basic-concepts/Program.cs
@@ -6,27 +6,30 @@ namespace basic_concepts
     {
         static void Main(string[] args)
         {
-            BasicSyntax bs = new BasicSyntax();
-            ExceptionHandling eh = new ExceptionHandling();
-            LoopsAndInteraction li = new LoopsAndInteraction();
-            MethodsAndExpressions me = new MethodsAndExpressions();
-            Numbers nm = new Numbers();
-
             Console.WriteLine((("----- Basic Syntax ------").ToUpper()));
+            BasicSyntax bs = new BasicSyntax();
             bs.WorkingWithStrings();
             bs.WorkingWithBool(27);
 
             Console.WriteLine((("----- Exception Handling ------").ToUpper()));
+            ExceptionHandling eh = new ExceptionHandling();
             eh.HandlingTypes();
 
             Console.WriteLine((("----- Loops And Interaction ------").ToUpper()));
+            LoopsAndInteraction li = new LoopsAndInteraction();
             li.LoopsAndInteractionTypes();
 
             Console.WriteLine((("----- Methods And Expressions Types ------").ToUpper()));
+            MethodsAndExpressions me = new MethodsAndExpressions();
             me.MethodsAndExpressionsTypes();
 
             Console.WriteLine((("----- Numbers ------").ToUpper()));
+            Numbers nm = new Numbers();
             nm.NumbersTypes();
+
+            Console.WriteLine((("----- Dates ------").ToUpper()));
+            Dates dt = new Dates();
+            dt.DatesTypes();
         }
     }
 }
